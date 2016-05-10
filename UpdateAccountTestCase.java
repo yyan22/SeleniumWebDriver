7c81ab22d38ca87d71833650ffc6df42e3ffb137
yyan22 <yanyyj@gmail.com>
2016-05-09 23:07:00 -0500

    Update UpdateAccountTestCase.java

--- UpdateAccountTestCase.java
+++ UpdateAccountTestCase.java
@@ -9,7 +9,7 @@ public class UpdateAccountTestCase {
 	@Test
 	public void test() {
 		// The reason it is not implemented is because I registered online but have not received any confirmation email.
-		// As long as I get the link to login, I will work on this test case immediately.
+		// As long as I get the link to login, I will get to this test case immediately.
 		fail("Not yet implemented");
 	}
 
