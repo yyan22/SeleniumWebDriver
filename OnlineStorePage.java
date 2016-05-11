122cd80f077a76910aa4068bf987e0e95a4cb4a4
yyan22 <yanyyj@gmail.com>
2016-05-11 07:23:11 -0700

    Update OnlineStorePage.java

--- OnlineStorePage.java
+++ OnlineStorePage.java
@@ -3,52 +3,51 @@ package automationFramework;
 import org.openqa.selenium.*;
 
 public class OnlineStorePage {
-    
-	protected WebDriver driver;
-	protected JavascriptExecutor executor;
+    protected WebDriver driver;
+    protected JavascriptExecutor executor;
 	
-	public OnlineStorePage(WebDriver driver) {
-	    this.driver = driver;
-	    this.executor = (JavascriptExecutor) driver;
-	}
+    public OnlineStorePage(WebDriver driver) {
+	this.driver = driver;
+	this.executor = (JavascriptExecutor) driver;
+    }
 	
-	public void open(String url) {
-		driver.get(url);
-	}
+    public void open(String url) {
+	driver.get(url);
+    }
 	
-	public void close() {
-		driver.quit();
-	}
+    public void close() {
+	driver.quit();
+    }
 	
-	public void typeUserInformation(WebElement elem, String userInput) {
-		elem.click();
-		elem.clear();
-		elem.sendKeys(userInput);
-		elem.sendKeys(Keys.TAB);
-		//executor.executeScript("$(arguments[0]).change();", elem);
-	}
+    public void typeUserInformation(WebElement elem, String userInput) {
+	elem.click();
+	elem.clear();
+	elem.sendKeys(userInput);
+	elem.sendKeys(Keys.TAB);
+	//executor.executeScript("$(arguments[0]).change();", elem);
+    }
 	
-	public void submit(WebElement elem) {
-	    elem.submit();
-	}
+    public void submit(WebElement elem) {
+	elem.submit();
+    }
 	
-	public void login() {
-	    WebElement username = driver.findElement(By.id("log"));
-	    this.typeUserInformation(username, "yyan");
-	    WebElement password = driver.findElement(By.id("pwd"));
-	    this.typeUserInformation(password, "Happy4ever");
-	    WebElement loginElem = driver.findElement(By.id("login"));
-		executor.executeScript("arguments[0].click();", loginElem);
-	}
+    public void login() {
+	WebElement username = driver.findElement(By.id("log"));
+	this.typeUserInformation(username, "yyan");
+	WebElement password = driver.findElement(By.id("pwd"));
+	this.typeUserInformation(password, "Happy4ever");
+	WebElement loginElem = driver.findElement(By.id("login"));
+        executor.executeScript("arguments[0].click();", loginElem);
+    }
 	
-	public void logout() {
-		WebElement logoutElem = driver.findElement(By.linkText("(Logout)"));
-		executor.executeScript("arguments[0].click();", logoutElem);
-	}
+    public void logout() {
+	WebElement logoutElem = driver.findElement(By.linkText("(Logout)"));
+	executor.executeScript("arguments[0].click();", logoutElem);
+    }
 	
-	public void addProduct(WebElement elem) {
-		executor.executeScript("arguments[0].click();", elem);
-	}
+    public void addProduct(WebElement elem) {
+	executor.executeScript("arguments[0].click();", elem);
+    }
 	
     public void removeProduct(WebElement elem) {
     	elem.click();
