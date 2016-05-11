0d234c360428b7e873a841293d2997b2c4b6e78f
yyan22 <yanyyj@gmail.com>
2016-05-11 08:35:42 -0500

    Update OnlineStorePage.java

--- OnlineStorePage.java
+++ OnlineStorePage.java
@@ -3,7 +3,7 @@ package automationFramework;
 import org.openqa.selenium.*;
 
 public class OnlineStorePage {
-	
+
 	protected WebDriver driver;
 	protected JavascriptExecutor executor;
 	
@@ -21,8 +21,29 @@ public class OnlineStorePage {
 	}
 	
 	public void typeUserInformation(WebElement elem, String userInput) {
+		elem.click();
 		elem.clear();
 		elem.sendKeys(userInput);
+		elem.sendKeys(Keys.TAB);
+		//executor.executeScript("$(arguments[0]).change();", elem);
+	}
+	
+	public void submit(WebElement elem) {
+	    elem.submit();
+	}
+	
+	public void login() {
+	    WebElement username = driver.findElement(By.id("log"));
+	    this.typeUserInformation(username, "yyan");
+	    WebElement password = driver.findElement(By.id("pwd"));
+	    this.typeUserInformation(password, "Happy4ever");
+	    WebElement loginElem = driver.findElement(By.id("login"));
+		executor.executeScript("arguments[0].click();", loginElem);
+	}
+	
+	public void logout() {
+		WebElement logoutElem = driver.findElement(By.linkText("(Logout)"));
+		executor.executeScript("arguments[0].click();", logoutElem);
 	}
 	
 	public void addProduct(WebElement elem) {
