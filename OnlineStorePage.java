2247d6b1cef0cbe115d17c5a7d2a904a38edd937
yyan22 <yanyyj@gmail.com>
2016-05-11 09:00:23 -0700

    Update OnlineStorePage.java

--- OnlineStorePage.java
+++ OnlineStorePage.java
@@ -24,7 +24,6 @@ public class OnlineStorePage {
 	elem.clear();
 	elem.sendKeys(userInput);
 	elem.sendKeys(Keys.TAB);
-	//executor.executeScript("$(arguments[0]).change();", elem);
     }
 	
     public void submit(WebElement elem) {
